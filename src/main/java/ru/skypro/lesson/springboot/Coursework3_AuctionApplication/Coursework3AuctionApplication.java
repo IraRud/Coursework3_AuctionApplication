1b16217ea314ce4797f50c57a94b0309dbeb3ad1
rudskaya irina <irarudskaya5@gmail.com>
2023-09-17 20:29:20 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lesson/springboot/Coursework3_AuctionApplication/Coursework3AuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lesson.springboot.Coursework3_AuctionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3AuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework3AuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/lesson/springboot/Coursework3_AuctionApplication/Coursework3AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lesson.springboot.Coursework3_AuctionApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework3AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
